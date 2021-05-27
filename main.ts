85a08bd2aea33917eccef545d88a798b62ac6500
jordicollell <78138546+jordicollell@users.noreply.github.com>
2021-05-27 13:46:11 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "x") {
+        endavant = value / 11
+    }
+    if (name == "y") {
+        gir = value / 11
+    }
+    cuteBot.motors(endavant - gir, endavant + gir)
+})
+let gir = 0
+let endavant = 0
+radio.setGroup(1)
